5983d81abf00867349c7ce6628e2e51c8de9d6b2
anmolverma18 <133114646+anmolverma18@users.noreply.github.com>
2023-05-10 23:09:12 +0530

    Update Solution.cpp

--- Solution.cpp
+++ Solution.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    void dfs(vector<int> adj[],int i,vector<int>&vis){
+    void dfs(vector<int> adj[],int i,vector<bool>&vis){   //dfs
         for(auto k : adj[i]){
             if(!vis[k]){
-                vis[k]=1;
+                vis[k]=true;
                 dfs(adj,k,vis);
             }
         }
     }
-    int makeConnected(int n, vector<vector<int>>& v) {
+    int driverfunction(int n, vector<vector<int>>& v) { //used to check the total no of components
         if(v.size()<n-1)
         {
             return -1;
         }
         vector<int> adj[n];
-        vector<int>vis(n,0);
+        vector<bool>vis(n,false);
         int c=0;
-        for(auto i:v){
-            adj[i[0]].push_back(i[1]);
-            adj[i[1]].push_back(i[0]);
+        for(int i=0;i<v.size();i++){
+            adj[v[i][0]].push_back(v[i][1]);
+            adj[v[i][1]].push_back(v[i][0]);
         }
         for(int i=0;i<n;i++){
               if(!vis[i]){
-                  vis[i]=1;
+                  vis[i]=true;
                   c++;
                   dfs(adj,i,vis);
               }
